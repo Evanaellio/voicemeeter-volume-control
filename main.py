012aead1d719aa4ee9ec0bc2b94265e23e4bfee9
Evanaellio <evan.mottais@gmail.com>
2021-02-03 21:04:50 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,43 @@
+import voicemeeter
+from ctypes import cast, POINTER
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+import time
+from PIL import Image
+import pystray
+import threading
+
+kind = 'banana'
+voicemeeter.launch(kind)
+
+devices = AudioUtilities.GetSpeakers()
+interface = devices.Activate(
+    IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+volume = cast(interface, POINTER(IAudioEndpointVolume))
+
+VOLUME_DB_SHIFT = -15
+
+
+def control_voicemeeter_volume():
+    with voicemeeter.remote(kind) as vmr:
+        while True:
+            time.sleep(0.1)  # in seconds
+            new_volume = volume.GetMasterVolumeLevel() + VOLUME_DB_SHIFT
+            vmr.outputs[0].gain = new_volume
+
+
+def exit_app():
+    icon.stop()
+
+
+TRAY_TOOLTIP = 'Voicemeeter Volume Control'
+TRAY_ICON = 'icon_voicemeeter.png'
+icon = pystray.Icon(TRAY_TOOLTIP, Image.open(TRAY_ICON), menu=pystray.Menu(
+    pystray.MenuItem('Exit ' + TRAY_TOOLTIP, exit_app)
+))
+
+control_thread = threading.Thread(target=control_voicemeeter_volume, daemon=True)
+
+if __name__ == '__main__':
+    control_thread.start()
+    icon.run()
